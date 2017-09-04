b5e7a382aeb02db88901a5f6e666d1a156315d87
Dylan Katz <dylanishappy1@gmail.com>
2017-09-04 12:04:23 -0600

    Added better colors for install script

--- install.sh
+++ install.sh
@@ -2,16 +2,31 @@
 echo "Installing dependencies..."
 sudo apt-get install fish git
 
-curl -L "https://get.oh-my.fish" | fish
+function windir() {
+    echo $1 | sed -E 's+^/mnt/(.{1})+\1:+' | sed 's+:$+:/+1' 
+}
 
-echo "Linking shortcuts..."
+echo -e "\e[93m>>>\e[0m Installing fonts (This will take a while)..."
+git clone https://github.com/powerline/fonts.git --depth=1
+powershell.exe -executionpolicy bypass -File install.ps1
+
+echo "Installing oh-my-fish if not installed..."
+if [[ -f "~/.local/share/omf/" ]]; then
+	echo -e "$(curl -L "https://get.oh-my.fish") --noninteractive -y" | fish
+fi
+
+echo -e "\e[93m>>>\e[0m Installing themes..."
+fish -c "omf install bobthefish; omf theme bobthefish; exit"
+
+echo -e "\e[93m>>>\e[0m Linking shortcuts..."
+$MY_DOCUMENTS = windir($(powershell.exe /c "[Environment]::GetFolderPath('MyDocuments')"))
 ln -sv "/mnt/c/Users/$USER/Documents" ~
 ln -sv "/mnt/c/Users/$USER/Desktop" ~
 #ln -sv "/mnt/c/Users/$USER/Pictures" ~
 #ln -sv "/mnt/c/Users/$USER/Videos" ~
 #ln -sv "/mnt/c/Users/$USER/Music" ~
 
-echo "Checking for old backup files..."
+echo -e "\e[93m>>>\e[0m Checking for old backup files..."
 if [[ ! -f ~/.bash_aliases_old && ! -f ~/.profile_old  && ! -f ~/.bashrc_old && ! -f ~/.gitconfig_old ]]; then
    echo "None found. Backing up existing files..."
    if [[ -f ~/.bash_aliases ]]; then
@@ -21,11 +36,11 @@ if [[ ! -f ~/.bash_aliases_old && ! -f ~/.profile_old  && ! -f ~/.bashrc_old && 
    cp ~/.bashrc ~/.bashrc_old
    cp ~/.gitconfig ~/.gitconfig_old
 else
-   echo "Found existing backups. Will not overwrite them..."
+   echo -e "\e[93m>>>\e[0m Found existing backups. Will not overwrite them..."
 fi
 
 LOC=$PWD
-echo "Checking for updates..."
+echo -e "\e[93m>>>\e[0m Checking for updates..."
 git pull
 
 if [[ ! -d ~/wsl-dotfiles ]]; then
@@ -34,19 +49,19 @@ if [[ ! -d ~/wsl-dotfiles ]]; then
 
 else   
    cd ~/wsl-dotfiles
-   echo "Existing installation found. Using that..."
+   echo -e "\e[93m>>>\e[0m Existing installation found. Using that..."
 fi
 
-echo "Updating..."
+echo -e "\e[93m>>>\e[0m Updating..."
 git reset --hard origin/master
 git pull
 
-echo "Cleaning unused files..."
+echo -e "\e[93m>>>\e[0m Cleaning unused files..."
 rm -f ~/.profile ~/.bashrc ~/.gitconfig ~/.bash_aliases
-echo "Linking new files..."
+echo -e "\e[93m>>>\e[0m Linking new files..."
 ln -sv "/home/$USER/wsl-dotfiles/.profile" ~
 ln -sv "/home/$USER/wsl-dotfiles/.bashrc" ~
 ln -sv "/home/$USER/wsl-dotfiles/git/.gitconfig" ~
 ln -sv "/home/$USER/wsl-dotfiles/.bash_aliases" ~
 cp -rs "/home/$USER/wsl-dotfiles/.config" ~/.config
-echo "Finished!"
+echo -e "\e[93m>>>\e[0m Finished!"
